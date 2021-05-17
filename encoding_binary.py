f5c528dcefc0aa64cd5e639d9ef4b6cf93decc10
Michele <42770845+mikeleord@users.noreply.github.com>
2021-05-17 21:45:48 +0300

    Update encoding_binary.py

--- encoding_binary.py
+++ encoding_binary.py
@@ -1,5 +1,9 @@
+# this script converts the ncat.exe file (located in the same folder) to base64
+#run $> python3 encoding_binary.py
+
 import base64
 
+# you can change the file name here
 with open('ncat.exe', 'rb') as binary_file:
     binary_file_data = binary_file.read()
     base64_encoded_data = base64.b64encode(binary_file_data)
@@ -7,3 +11,4 @@ with open('ncat.exe', 'rb') as binary_file:
 
     print(base64_message)
 
+# copy and paste the output into a file
