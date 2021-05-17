269168c7112cef45aa14625d683c91eb7aa61508
Michele <42770845+mikeleord@users.noreply.github.com>
2021-05-17 21:53:56 +0300

    Update 2-ncatbase64M.sh

--- 2-ncatbase64M.sh
+++ 2-ncatbase64M.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#the command transforms all the ¨As of the ncatbase64 file into ¨M¨
-sed 's/A/M/g' prova1 > prova3
+#the command transforms all the ¨A¨ of the ncatbase64 file into ¨M¨, and save the ncatbase64M
+sed 's/A/M/g' ncatbase64 > ncatbase64M
