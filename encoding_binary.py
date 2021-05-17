6dd2d082936053d6aa68ea2e051fec3915888200
Michele <42770845+mikeleord@users.noreply.github.com>
2021-05-17 21:42:06 +0300

    Add files via upload

---
+++ encoding_binary.py
@@ -0,0 +1,9 @@
+import base64
+
+with open('ncat.exe', 'rb') as binary_file:
+    binary_file_data = binary_file.read()
+    base64_encoded_data = base64.b64encode(binary_file_data)
+    base64_message = base64_encoded_data.decode('utf-8')
+
+    print(base64_message)
+
